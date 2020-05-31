5bbc9c0ac33f45d0197b106def431c26c7967753
grahamgamedev <54633859+grahamgamedev@users.noreply.github.com>
2020-05-31 16:18:06 +0100

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Today is $(date)
+echo You are in $PWD and your host is $HOSTNAME
+echo ""
+echo There are $# peramiters that include $@
+echo the first is $1 the second is $2 and the third is $3
